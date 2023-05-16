00ef34c50c096c03d02c41b5e4b7b799e1b360f9
kcryns <rayneskarlcydrick@gmail.com>
2023-05-16 15:30:44 +0800

    1st commit

---
+++ dist/js/script.js
@@ -0,0 +1,43 @@
+// const toggleMenu = document.querySelector(".toggle__menu");
+// const headerBot = document.querySelector(".header__nav");
+// toggleMenu.addEventListener("click", () => {
+//   toggleMenu.classList.toggle("open");
+//   headerBot.classList.toggle("open");
+// });
+
+
+// const headerb =document.querySelector(".header") ;
+// window.addEventListener("scroll", () => {
+//   console.log(window.pageYOffset);
+//   if(window.pageYOffset >=100){
+//     headerb.classList.add("shadowb")
+//   }
+//   else{
+//     headerb.classList.remove("shadowb")
+//   }
+// })
+
+// ----COFFE TAB----//
+const starNav = document.querySelectorAll(".star__nav__link");
+const starContent = document.querySelectorAll(".coffee__tab__content");
+starNav.forEach((star) => {
+  star.addEventListener("click", () => {
+    removeActiveStar();
+    star.classList.add("active");
+    const activeContent = document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveStar() {
+  starNav.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  starContent.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
